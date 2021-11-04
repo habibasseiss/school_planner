66ae82b8c5b8c4819dc3eae83a5a51a0e7ed0037
Habib Asseiss Neto <habibasseiss@gmail.com>
2021-11-04 16:16:37 -0400

    app localization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('pt', ''), // Portuguese
           ],
 
           // Use AppLocalizations to configure the correct application title
