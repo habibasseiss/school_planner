e69412e8ebd24d0634355908b234aa95e40aebf0
Felipe Delite <felipedelite@gmail.com>
2021-11-17 18:33:41 -0300

    add teacher form class

---
+++ lib/src/forms/teacher.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TeacherForm extends StatefulWidget {
+  const TeacherForm({Key? key}) : super(key: key);
+
+  @override
+  _TeacherFormState createState() => _TeacherFormState();
+}
+
+class _TeacherFormState extends State<TeacherForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(),
+    );
+  }
+}
